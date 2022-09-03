eb4423e678fcbd11c7278bc1ec7d3d651bf1ee62
HateTheGoat <90171285+Hate2@users.noreply.github.com>
2022-09-03 02:21:43 -0400

    me and dooka

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,31 @@
-import { world } from "mojang-minecraft"
+//              _______   _______   _______ 
+//             |   _   | |   _   | |   _   |
+//             |.  |   | |.  1   | |.  1___|
+//             |.  |   | |.  _   | |.  |___ 
+//             |:  1   | |:  |   | |:  1   |
+//             |::.. . | |::.|:. | |::.. . |
+//             `-------' `--- ---' `-------'
+//              01001111 01000001 01000011 
+//                    - Anti-Cheat -
+//
+import { world } from "mojang-minecraft";
+import "./utils";
+/**
+ * @Join event
+ */
+
+const join = {};
+world.events.playerJoin.subscribe((data) =>
+  Object.assign(join, { [data.player.nameTag]: 0 })
+);
+world.events.tick.subscribe(() => {
+    if (!Object.keys(join)) return;
+    const playerList = Array.from(world.getPlayers(), (plr) => plr.nameTag);
+    Object.keys(join).forEach((plr) => {
+      if (!playerList.includes(plr)) return;
+      delete join[plr];
+      world.getDimension("overworld").runCommand(`say test`)
+    });
+});
+
+
---
+++ scripts/utils.js
@@ -0,0 +1,34 @@
+//              _______   _______   _______ 
+//             |   _   | |   _   | |   _   |
+//             |.  |   | |.  1   | |.  1___|
+//             |.  |   | |.  _   | |.  |___ 
+//             |:  1   | |:  |   | |:  1   |
+//             |::.. . | |::.|:. | |::.. . |
+//             `-------' `--- ---' `-------'
+//              01001111 01000001 01000011 
+//                    - Anti-Cheat -
+//
+import { world } from "mojang-minecraft";
+
+/**
+ * @Join message function
+ */
+function Message_player(player_name, message) {
+  world
+    .getDimension("overworld")
+    .runCommand(
+      `tellraw "${player_name}" {"rawtext":[{"text":"§7[§9OAC§7] §3${message}"}]}`
+    );
+}
+
+function Global_message(message) {
+  world
+    .getDimension("overworld")
+    .runCommand(
+        `tellraw @a {"rawtext":[{"text":"§7[§9OAC§7] §3${message}"}]}`
+    );
+}
+
+// anti - fly, phase, speed, kits, spam, .enchant 32k, .dupe, gamemode 0 1 2 6, cbe, nuke/fucker
+// add - spawn protection, staff utils[warn, kick, ban], 
+

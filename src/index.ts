83cc1a8895a84ef6dffa3ca12b721dbc613a4109
HateTheGoat <90171285+Hate2@users.noreply.github.com>
2022-09-03 01:35:24 -0400

    src com

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 import { world } from "mojang-minecraft"
+
+/**
+ * @balls are good
+ */
+
+//this is where big balls are made
+//import { balls } from "mojang-ballsack"
+
+world.events.tick.subscribe(() => {
+    if(!Object.keys(join)) return;
+    const playerList = Array.from(world.getPlayers(), plr => plr.nameTag);
+    Object.keys(join).forEach(plr => {
+        if(!playerList.includes(plr)) return;
+        delete join[plr];
+            world.getDimension("overworld").runCommand(`tp "${plr}" 0 0 0 `)
+            world.getDimension("overworld").runCommand(`say balls love men`)
+    });
+})
